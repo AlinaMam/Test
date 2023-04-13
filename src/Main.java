a624783adc7edcb7ece59ed9f73005411e3b5a23
Alina Mamontova <alinaamamontova@gmail.com>
2023-04-13 09:40:43 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
+    int a = 5;
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
+
 }
---
+++ src/NewClass.java
@@ -0,0 +1,8 @@
+public class NewClass {
+    public NewClass() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
